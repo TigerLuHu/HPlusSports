9286524e3529961e204da47537549786fc817c7c
TigerLuHu <luhu_feng@163.com>
2024-01-30 00:34:04 +0800

    fix product detail page

--- HPlusSportsWeb/Pages/Product/Detail.cshtml.cs
+++ HPlusSportsWeb/Pages/Product/Detail.cshtml.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Newtonsoft.Json;
 
 namespace HPlusSportsWeb.Pages.Product
 {
     public class DetailModel : PageModel
     {
-        [BindProperty]
+        private HttpClient _httpClient;
+
+        public DetailModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public HPlusSports.Shared.Models.Product Product { get; set; }
+
+        public async Task OnGetAsync(int id)
+        {
+            var response = await _httpClient.GetStringAsync($"product/{id}");
+            Product = JsonConvert.DeserializeObject<HPlusSports.Shared.Models.Product>(response);
+        }
     }
 }
