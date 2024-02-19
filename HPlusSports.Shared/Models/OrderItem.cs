6f75d28f14ccbb6a7c8b7a2c7a2bd417e1f4c920
TigerLuHu <luhu_feng@163.com>
2024-02-20 01:25:27 +0800

    minor

--- HPlusSports.Shared/Models/OrderItem.cs
+++ HPlusSports.Shared/Models/OrderItem.cs
@@ -4,6 +4,8 @@
     {
         public string Name { get; set; }
 
+        public string Category { get; set; }
+
         public int Quantity { get; set; }
 
         public string Size { get; set; }
