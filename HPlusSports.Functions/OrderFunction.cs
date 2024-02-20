41ced324ccc7eb8d4d19028332e998feeb2f5b07
TigerLuHu <luhu_feng@163.com>
2024-02-21 00:39:05 +0800

    using Microsoft.Azure.Functions.Worker.Extensions.Tables instead of Microsoft.Azure.WebJobs.Extensions.Storage, while TableOutput binding doesn't work in local debuging, will try in azure.

--- HPlusSports.Functions/OrderFunction.cs
+++ HPlusSports.Functions/OrderFunction.cs
@@ -3,48 +3,39 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
-// using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker;
 using HPlusSports.Functions.Models;
 using System.Collections.Generic;
 using System;
-using Microsoft.AspNetCore.Mvc;
 
 namespace HPlusSports.Functions
 {
     public static class OrderFunction
     {
         [FunctionName("OrderFunction")]
-        // [TableOutput("orderhistory", Connection = "AzureWebJobsStorage")]
-        public static async Task<IActionResult> Run(
+        [TableOutput("orderhistory", Connection = "AzureWebJobsStorage")]
+        public static async Task<OrderHistoryEntity[]> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req,
-            [Table("orderhistory")] ICollector<OrderHistoryEntity> entities,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            log.LogInformation(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
 
-            try
+            var order = await req.Content.ReadAsAsync<Order>();
+
+            var orderHistoryEntities = new List<OrderHistoryEntity>();
+            foreach (var item in order.Items)
             {
-                var order = await req.Content.ReadAsAsync<Order>();
+                var orderHistoryEntity = new OrderHistoryEntity(item);
+                orderHistoryEntity.PartitionKey = item.Category;
+                orderHistoryEntity.RowKey = $"{order.Id}-{item.Id}";
 
-                var orderHistoryEntities = new List<OrderHistoryEntity>();
-                foreach (var item in order.Items)
-                {
-                    var orderHistoryEntity = new OrderHistoryEntity(item);
-                    orderHistoryEntity.PartitionKey = item.Category;
-                    orderHistoryEntity.RowKey = $"{order.Id}-{item.Id}";
-                    orderHistoryEntities.Add(orderHistoryEntity);
+                orderHistoryEntity.Timestamp = DateTime.UtcNow;
+                orderHistoryEntity.ETag = DateTime.UtcNow.Ticks.ToString();
 
-                    entities.Add(orderHistoryEntity);
-                }
-            }
-            catch (Exception ex) 
-            {
-                log.LogError(ex.ToString());
+                orderHistoryEntities.Add(orderHistoryEntity);
             }
 
-            // return orderHistoryEntities.ToArray();
-            return new OkResult();
+            return orderHistoryEntities.ToArray();
         }
     }
 }
